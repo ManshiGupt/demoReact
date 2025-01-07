dffb643ed23838bce20d31058dee2918b13a1b86
ManshiGupt <heymanshigupta@gmail.com>
2025-01-07 11:27:35 +0530

    dd

--- src/components/Context.js
+++ src/components/Context.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from 'react'
 const theamContext= createContext();
 const Context = ({children}) => {
    
-    const[t,setT]= useState("lidght");
+    const[t,setT]= useState("light");
     const handleToggle=()=>{
         setT((prev)=>
            ( prev === "light"? "dack":"light")
